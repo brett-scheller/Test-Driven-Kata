7ba5dac622f0a834ae69ba7a4e1ff7a700794197
Brett Scheller <brett.green.scheller@gmail.com>
2018-10-01 10:11:59 -0500

    starting serious tests

---
+++ wrap.spec.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const wrap = require('./wrap');
+
+describe('wrap', () => {
+  it('Returns an empty string if empty string was provided', () => {
+    expect(wrap('', 10)).to.equal('');
+  });
+  it('Works on longer strings', () => {
+    expect(wrap('hello hello hello hello hello', 5)).to.equal('hello \nhello \nhello \nhello \nhello')
+  })
+});
